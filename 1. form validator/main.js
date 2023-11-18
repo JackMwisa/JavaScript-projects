8f22a167c2533e00afad82ad54b4f8688480408a
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-11-18 13:25:37 +0300

    Update main.js

--- 1. form validator/main.js
+++ 1. form validator/main.js
@@ -4,9 +4,32 @@ const username = document.getElementById('username');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+
+// Show input error message
+function showError(input, message){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small')
+    small.innerText = message;
+}
+
+//show success 
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+
+// Event listeners
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    console.log(username.value);
-    
+    // console.log(username.value);
+
+    if (username.value === ""){
+        showError(username, "Username is required");
+    } else {
+        showSuccess(username);
+    }
 })
