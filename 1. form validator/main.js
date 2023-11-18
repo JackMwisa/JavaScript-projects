a36ca42e622ba42fb64e3edd0d4eeb47a9b2a5d5
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-11-18 13:36:58 +0300

    Update main.js

--- 1. form validator/main.js
+++ 1. form validator/main.js
@@ -4,32 +4,56 @@ const username = document.getElementById('username');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-
 // Show input error message
-function showError(input, message){
+function showError(input, message) {
     const formControl = input.parentElement;
     formControl.className = 'form-control error';
-    const small = formControl.querySelector('small')
+    const small = formControl.querySelector('small');
     small.innerText = message;
 }
 
-//show success 
-function showSuccess(input){
+// Show success message
+function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
 }
 
+// Check valid email
+function checkEmail(input) {
+    const re = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
+}
 
 // Event listeners
-
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    // console.log(username.value);
-
-    if (username.value === ""){
-        showError(username, "Username is required");
+    if (username.value === '') {
+        showError(username, 'Username is required');
     } else {
         showSuccess(username);
     }
-})
+
+    if (email.value === '') {
+        showError(email, 'Email is required');
+    } else {
+        checkEmail(email);
+    }
+
+    if (password.value === '') {
+        showError(password, 'Password is required');
+    } else {
+        showSuccess(password);
+    }
+
+    if (password2.value === '') {
+        showError(password2, 'Password is required');
+    } else {
+        showSuccess(password2);
+    }
+});
+
